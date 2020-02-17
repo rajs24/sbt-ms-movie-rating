74ece0f333d990da840f029acf838393099fef10
Arockiaraj Savarinathan <arockiarajs24@gmail.com>
2020-02-17 22:51:36 +0800

    project upload

---
+++ src/test/java/com/sbt/ms/movie/rating/MovieRatingTests.java
@@ -0,0 +1,13 @@
+package com.sbt.ms.movie.rating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRatingTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
